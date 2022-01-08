0da14d60630240cc364195569e7e8e7954fcbfd3
auifzysr <auifzysr@gmail.com>
2022-01-09 00:14:38 +0900

    Change params by argv

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const puppeteer = require('puppeteer');
 
+if (process.argv.length !== 4){
+  process.exit(1);  
+}
+
+const url = process.argv[2];
+const outDir = process.argv[3];
+
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto(url);
   await page.screenshot({
-    path: 'example.png'
+    path: `${outDir}example.png`
   });
 
   await browser.close();
