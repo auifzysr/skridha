4f257ee8034dc9cb22dd2e9691dadebc636f3cda
auifzysr <auifzysr@gmail.com>
2022-01-09 23:44:24 +0900

    Extract controller

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,11 @@
-import puppeteer from 'puppeteer';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { Response } from 'express';
-
-import { RequestWithQueryParams } from './types/request-with-query-params';
+import * as apiController from './controllers/api';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("port", process.env.PORT || 3000);
-app.get("/api", (req: RequestWithQueryParams, res: Response) => {
-  const url = req.query.url;
-  const outDir = req.query.to;
-
-  res.send("test");
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url || 'https://example.com');
-    await page.screenshot({
-      path: `${outDir || "."}example.png`
-    });
-    await browser.close();
-  })();
-});
+app.get("/api", apiController.getApi);
 
 export default app;
