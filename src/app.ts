5c7e68ce953fafef45cd466e49501d87310e82d0
auifzysr <auifzysr@gmail.com>
2022-01-18 22:02:36 +0900

    Prtsc and post anyhow

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import * as apiController from './controllers/api';
 import dotenv from 'dotenv';
 import path from 'path';
 
+import puppeteer from 'puppeteer'
+const { WebClient, LogLevel } = require("@slack/web-api");
+
 const confResult = dotenv.config({
   path: path.resolve(process.cwd(), ".env"),
 });
@@ -16,5 +19,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("port", Number(process.env.PORT) || 9000);
 app.get("/api", apiController.getApi(process.env.SLACK_BOT_TOKEN as string, process.env.SLACK_SIGNING_SECRET as string));
+
+const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN, {
+  logLevel: LogLevel.DEBUG
+});
+
+app.post('/api/prtsc', async (req, res, next) => {
+  if(!req.query.url || !req.query.channel_id){
+    const err = new Error('url is required');
+    res.status(400);
+    return next(err);
+  }
+
+  await res.status(201).json();
+
+  let screenshot_options = {
+    fullPage: true
+  }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(req.query.url as string);
+  const buf = await page.screenshot(screenshot_options) as Buffer;
+  await browser.close();
+
+  try {
+    const result = await slackClient.files.upload({
+      channels: req.query.channel_id,
+      initial_comment: "prtsc invoked by api",
+      file: buf
+    });
+    console.log(result);
+
+  } catch (error) {
+    console.error(error);
+  }
+
+})
 
 export default app;
